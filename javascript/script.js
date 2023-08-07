5f083276b5b5c22bf58ccd6bface620f225ac9a8
relieze <127432944+relieze@users.noreply.github.com>
2023-08-06 22:05:13 -0700

    Make template loop that creates 16x16 square grid

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+const body = document.querySelector("body");
+const gridBox = document.querySelector(".gridBox");
+const rows = 16;
+const columns = 16;
+
+for (let i = 0; i < rows; i++) {
+  for (let e = 0; e < columns; e++) {
+    const square = document.createElement("div");
+  }
+}
