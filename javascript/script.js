c2e0a746e5af402862de42bd9add302fe264cac5
relieze <127432944+relieze@users.noreply.github.com>
2023-08-07 11:10:51 -0700

    Add button for grid size assignment + relevant CSS

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 const body = document.querySelector("body");
 const gridBox = document.querySelector(".gridBox");
-const gridColumns = 16;
 const gridRows = 16;
+const gridColumns = 16;
 const gridArea = gridRows * gridColumns;
 
 for (let i = 0; i < gridArea; i++) {
