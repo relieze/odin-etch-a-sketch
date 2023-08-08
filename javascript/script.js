4604a915af75bc110fedf8f4fa9753124c80e943
relieze <127432944+relieze@users.noreply.github.com>
2023-08-07 21:57:30 -0700

    Use loop to add click listener to all btns
    The loop will make it so that when a button is pressed, a
    class of "on" will be added to it, and all other buttons
    will lose that class.
    
    Also:
      - change color format from rgb to hex: this will make it easier
        to adjust shades using the buttons later on; you just have to
        change the lightness value

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,15 @@
 const gridBox = document.querySelector(".gridBox");
 
 // buttons
+const buttons = document.querySelectorAll(".buttons button");
 const gridSizeBtn = document.querySelector(".gridSize");
 const randomFillBtn = document.querySelector(".randomFill");
+const lightShadeBtn = document.querySelector(".lightShade");
+const darkShadeBtn = document.querySelector(".darkShade");
 
 let gridRows = 16, gridColumns = 16;
 createGrid(gridRows * gridColumns);
-colorOnHover();
+handleHover();
 
 function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
@@ -18,7 +21,17 @@ function createGrid(gridSize) {
 }
 
 gridSizeBtn.addEventListener("click", changeGridSize);
-randomFillBtn.addEventListener("click", event => event.target.classList.toggle("on"));
+
+buttons.forEach(function(btn) {
+    btn.addEventListener("click", event => clearOtherButtons(event.target));
+})
+
+function clearOtherButtons(target) {
+    buttons.forEach((btn) => {
+        target === btn && target != gridSizeBtn ? 
+        btn.classList.toggle("on") : btn.classList.remove("on");
+    })
+}
 
 function changeGridSize() {
     gridRows = prompt("What width do you want for the grid?");
@@ -26,25 +39,25 @@ function changeGridSize() {
     
     while (gridBox.firstChild) gridBox.removeChild(gridBox.firstChild);
     createGrid(gridRows * gridColumns);
-    colorOnHover();
+    handleHover();
 }
 
-function colorOnHover() {
+function handleHover() {
     const squares = document.querySelectorAll(".gridBox div");
 
     squares.forEach(function(square) {
         square.addEventListener("mouseover", event => changeColor(event));
     });
+}
 
-    function changeColor(event) {
-        let color = "black";
-        if (randomFillBtn.classList.contains("on")) color = randomColor();
+function changeColor(event) {
+    let color = "hsl(0, 0%, 0%)";
+    if (randomFillBtn.classList.contains("on")) color = randomColor();
 
-        event.target.style.backgroundColor = color;
-    }
+    event.target.style.backgroundColor = color;
 }
 
 function randomColor() {
-    function randNum() {return Math.floor(Math.random() * 256);}
-    return (`rgb(${randNum()}, ${randNum()}, ${randNum()})`);
+    function randNum(max) {return Math.floor(Math.random() * max);}
+    return `hsl(${randNum(360)}, ${randNum(31) + 70}%, ${randNum(71) + 15}%)`;    
 }
