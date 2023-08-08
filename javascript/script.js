f293b07ae1fb372a1e6fbdb67f8d2442ccb8f12f
relieze <127432944+relieze@users.noreply.github.com>
2023-08-08 02:12:24 -0700

    Add clear btn + functionality; add header title

--- javascript/script.js
+++ javascript/script.js
@@ -6,6 +6,7 @@ const gridSizeBtn = document.querySelector(".gridSize");
 const randomFillBtn = document.querySelector(".randomFill");
 const lightShadeBtn = document.querySelector(".lightShade");
 const darkShadeBtn = document.querySelector(".darkShade");
+const clearBtn = document.querySelector(".clear");
 
 let gridRows = 16, gridColumns = 16;
 createGrid(gridRows * gridColumns);
@@ -23,17 +24,7 @@ function createGrid(gridSize) {
 }
 
 gridSizeBtn.addEventListener("click", changeGridSize);
-
-buttons.forEach((btn) => {
-    btn.addEventListener("click", () => clearOtherButtons(btn));
-})
-
-function clearOtherButtons(target) {
-    buttons.forEach((btn) => {
-        target === btn && target != gridSizeBtn ? 
-        btn.classList.toggle("on") : btn.classList.remove("on");
-    })
-}
+clearBtn.addEventListener("click", clearGrid);
 
 function changeGridSize() {
     gridRows = prompt("What width do you want for the grid?");
@@ -42,6 +33,24 @@ function changeGridSize() {
     while (gridBox.firstChild) gridBox.removeChild(gridBox.firstChild);
     createGrid(gridRows * gridColumns);
     handleHover();
+}
+
+function clearGrid() {
+    document.querySelectorAll(".gridBox div").forEach((square) => {
+        square.dataset.color = "hsl(000, 000%, 100%)";
+        square.style.backgroundColor = square.dataset.color;
+    })
+}
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => clearOtherButtons(btn));
+})
+
+function clearOtherButtons(target) {
+    buttons.forEach((btn) => {
+        target === btn && target != gridSizeBtn && target != clearBtn ? 
+        btn.classList.toggle("on") : btn.classList.remove("on");
+    })
 }
 
 function handleHover() {
@@ -71,14 +80,14 @@ function randomColor() {
 function shadeEffect(square, color) {
     if (lightShadeBtn.classList.contains("on")) {
         color = square.dataset.color;
-        let lightnessValue = parseInt(color.slice(15,18));
-        if (lightnessValue < 100) lightnessValue += 10;
-        color = color.slice(0, 15) + `${lightnessValue}`.padStart(3, "0") + color.slice(18);
+        let lValue = parseInt(color.slice(15,18));
+        if (lValue < 100) lValue += 10;
+        color = color.slice(0, 15) + `${lValue}`.padStart(3, "0") + color.slice(18);
     } else if (darkShadeBtn.classList.contains("on")) {
         color = square.dataset.color;
-        let darknessValue = parseInt(color.slice(15,18));
-        if (darknessValue > 0) darknessValue -= 10;
-        color = color.slice(0, 15) + `${darknessValue}`.padStart(3, "0") + color.slice(18);
+        let lValue = parseInt(color.slice(15,18));
+        if (lValue > 0) lValue -= 10;
+        color = color.slice(0, 15) + `${lValue}`.padStart(3, "0") + color.slice(18);
     }
     return color;
 }
